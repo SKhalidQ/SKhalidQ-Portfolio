54b9d92c4853f2e7c571e34d864ca7b19440aba0
Khalid Qayyum, Sad <sad.khalidqayyum@weatherford.com>
2025-10-08 12:42:15 +0100

    Installed PWA and updated config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { TranslatePipe } from './pipes/translate/translate.pipe';
 
 import { DisableRightClickDirective } from './directives/DisableRightClick/disable-right-click.directive';
 import { DisableTextSelectionDirective } from './directives/DisableTextSelection/disable-text-selection.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,13 @@ import { DisableTextSelectionDirective } from './directives/DisableTextSelection
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModules
+    MaterialModules,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
