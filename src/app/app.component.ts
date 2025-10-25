136feb23e805dd73dcde1400fc85cd0269cc7388
Sad Khalid Qayyum <48598364+SKhalidQ@users.noreply.github.com>
2025-10-25 23:23:23 +0500

    Attempt to fix the reload-loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ThemeService } from './services/theme/theme.service';
-import { MetaThemeService } from './services/meta-theme/meta-theme.service';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter, map } from 'rxjs';
-import { Title } from '@angular/platform-browser';
 import { Page } from './models/enums/page';
-// import { SiteStatusService } from './services/site-status/site-status.service';
+import { MetaThemeService } from './services/meta-theme/meta-theme.service';
+import { SiteStatusService } from './services/site-status/site-status.service';
+import { ThemeService } from './services/theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -18,11 +18,11 @@ export class AppComponent implements OnInit {
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly router = inject(Router);
   private readonly titleService = inject(Title);
-  // private readonly siteStatusService = inject(SiteStatusService);
+  private readonly siteStatusService = inject(SiteStatusService);
 
   ngOnInit(): void {
     this.setTabTitle();
-    // this.siteStatusService.checkUpdates();
+    this.siteStatusService.checkUpdates();
   }
 
   setTabTitle(): void {
