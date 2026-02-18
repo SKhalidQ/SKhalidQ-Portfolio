9b411bba2c87d5f748c47a5268f46b45ed2f363e
Sad Khalid Qayyum <48598364+SKhalidQ@users.noreply.github.com>
2026-02-18 12:48:54 +0000

    Update about-credits.ts

--- src/app/models/data/about-credits.ts
+++ src/app/models/data/about-credits.ts
@@ -1,15 +1,17 @@
+import { VERSION as AngularVersion } from '@angular/core';
+import { VERSION as AngularMaterialVersion } from '@angular/material/core';
 import { AboutCredit } from '../interfaces/about-credit';
 
 export const AboutCredits: AboutCredit[] = [
   {
     title: 'Angular',
-    version: 'v16.2.14',
+    version: `v${AngularVersion.full}`,
     imagePath: '../../assets/images/logos/Angular-logo.png',
     pageUrl: 'https://angular.io/'
   },
   {
     title: 'Angular Material',
-    version: 'v16.2.14',
+    version: `v${AngularMaterialVersion.full}`,
     imagePath: '../../assets/images/logos/angular-material-logo.png',
     pageUrl: 'https://material.angular.io/'
   },
